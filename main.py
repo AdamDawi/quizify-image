15d60e2c98e3b0bc68fe720932af07a732b59d6e
Adam Dawidziuk <adamdawidziuk@onet.pl>
2025-04-16 10:25:11 +0200

    NEW: Execution time measurement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 from config import system_prompt
 from processing.formatter import process_image_to_json
 
@@ -8,9 +9,18 @@ def process_folder(folder_path, output_file, log_file):
             process_image_to_json(os.path.join(folder_path, filename), output_file, system_prompt, log_file)
 
 if __name__ == "__main__":
+    start_time = time.time()  # Start pomiaru czasu
+
     image_path = "data/test_images/i2.jpg"
     output_file = "output/formatted_questions.json"
     log_file = "output/deep_think_logs.txt"
-    process_folder("data/images", output_file, log_file)
+    # process_folder("data/images", output_file, log_file)
     
-    # process_image_to_json(image_path, output_file, log_file)
+    process_image_to_json(image_path, output_file, system_prompt, log_file)
+
+    end_time = time.time()  # Koniec pomiaru czasu
+    total_time = end_time - start_time
+    minutes = int(total_time // 60)
+    seconds = int(total_time % 60)
+    
+    print(f"Program execution time: {minutes} minute(s) {seconds} second(s)")
